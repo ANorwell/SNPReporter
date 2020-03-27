5ddfaf0ce3380726689542a9aa0523a91bd1a1f6
Arron Norwell <anorwell@gmail.com>
2020-03-27 08:47:34 -0400

    streaming iterator of extracted snp data; but error handling needs work

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -2,6 +2,7 @@ use reqwest::blocking::Client;
 use reqwest::Error;
 use serde::Deserialize;
 use serde::de::DeserializeOwned;
+
 use std::collections::HashMap;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,14 +11,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("list".to_string(), "categorymembers".to_string()),
         ("cmtitle".to_string(), "Category:Is_a_snp".to_string())];
     let request = MWRequest::query_json(params);
-    let pager: MWSource<HashMap<String, serde_json::Value>> = MWSource::new(&client, request);
-    pager.into_iter().take(3).for_each(|p| println!("{:?}", p));
+    let pager: MWSource<SNPListPage> = MWSource::new(&client, request);
+    let results: Vec<Result<Vec<SNPData>, Error>> = pager.into_iter().take(3).map(|page| handle_page(&client, page)).collect();
 
-    let req2 = MWRequest::get_titles(vec!("I3000078".to_string(), "I3000037".to_string()));
-    let rsp: MWResponse<serde_json::Value> = req2.send_request(&client)?;
-    println!("{:?}", rsp);
-
+    println!("{:?}", results);
     Ok(())
+}
+
+fn handle_page(client: &Client, page: Result<SNPListPage, Error>) -> Result<Vec<SNPData>,Error> {
+    let snps = page?.categorymembers.into_iter().map(|m| m.title).collect();
+    let req = MWRequest::get_titles(snps);
+    let rsp: MWResponse<SNPBatchPage> = req.send_request(&client)?;
+    Ok(rsp.query.pages.into_iter().map(|(k, v)| SNPData::parse(k, v) ).collect())
+}
+
+#[derive(Deserialize, Debug)]
+struct SNPListMember { title: String }
+#[derive(Deserialize, Debug)]
+struct SNPListPage { categorymembers: Vec<SNPListMember> }
+
+#[derive(Deserialize, Debug)]
+struct SNPBatchPage { pages: HashMap<String, serde_json::Value> }
+
+#[derive(Debug)]
+struct SNPData { name: String, content: String }
+impl SNPData {
+    fn parse(name: String, json: serde_json::Value) -> SNPData {
+        SNPData { name, content: json["revisions"][0]["slots"]["main"]["*"].as_str().unwrap().to_string()}
+    }
 }
 
 #[derive(Deserialize, Debug, Clone)]
