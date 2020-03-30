149f2a8c83963e7785cc73c8687cc7463e38d023
Arron Norwell <anorwell@gmail.com>
2020-03-30 08:18:08 -0400

    Add custom error type

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -3,6 +3,7 @@ use reqwest::Error;
 use serde::Deserialize;
 
 use std::collections::HashMap;
+use std::fmt;
 
 mod mediawiki;
 
@@ -24,8 +25,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn handle_page(client: &Client, page: Result<SNPListPage, Error>) -> Result<Vec<SNPData>,Error> {
     let snps = page?.categorymembers.into_iter().map(|m| m.title).collect();
     let req = MWRequest::get_titles(snps);
-    let rsp: MWResponse<SNPBatchPage> = req.send(&client)?;
-    Ok(rsp.query.pages.into_iter().map(|(k, v)| SNPData::parse(k, v) ).collect())
+    let rsp: MWResponse<SNPBatchPageSet> = req.send(&client)?;
+    Ok(rsp.query.pages.into_iter().flat_map(|(k, v)| SNPData::parse(k, v) ).collect())
 }
 
 #[derive(Deserialize, Debug)]
@@ -34,13 +35,31 @@ struct SNPListMember { title: String }
 struct SNPListPage { categorymembers: Vec<SNPListMember> }
 
 #[derive(Deserialize, Debug)]
-struct SNPBatchPage { pages: HashMap<String, serde_json::Value> }
+struct SNPBatchPageSet { pages: HashMap<String, serde_json::Value> }
+
+#[derive(Debug)]
+enum SNPError {
+    ParseError()
+}
+
+impl std::error::Error for SNPError {}
+
+impl fmt::Display for SNPError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }    
+}
+
 
 #[derive(Debug)]
 struct SNPData { name: String, content: String }
 impl SNPData {
-    fn parse(name: String, json: serde_json::Value) -> SNPData {
-        SNPData { name, content: json["revisions"][0]["slots"]["main"]["*"].as_str().unwrap().to_string() }
+    fn parse(name: String, json: serde_json::Value) -> Result<SNPData, SNPError> {
+        let content: Result<String, SNPError> = match json["revisions"][0]["slots"]["main"]["*"].as_str() {
+            None => Err(SNPError::ParseError()),
+            Some(str) => Ok(str.to_string())
+        };
+        content.map(|c| SNPData { name, content: c })
     }
 }
 
